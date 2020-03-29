c57a1f3ba3a971981477b4a25711fde54deb87d3
GraceL <graceliu0925@gmail.com>
2020-03-29 17:37:36 -0400

    Select cell

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ class App:
         self.window = pygame.display.set_mode((WIDTH, HEIGHT))
         self.running = True
         self.grid = BOARD
-        print(BOARD)
+        self.selected = None
+        self.mousePos = None
 
     # run the game
     def run(self):
@@ -23,21 +24,44 @@ class App:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected = self.mouse_clicked()
+                if selected:
+                    self.selected = selected
+                else:
+                    self.selected = None
 
-    def drawGrid(self, window):
+    def mouse_clicked(self):
+        if self.mousePos[0] < GRID_POSITION[0] or self.mousePos[0] > GRID_POSITION[0] + GRID_WIDTH:
+            return False
+        if self.mousePos[1] < GRID_POSITION[1] or self.mousePos[1] > GRID_POSITION[1] + GRID_HEIGHT:
+            return False
+        return (self.mousePos[1] - GRID_POSITION[1])//CELL_SIZE, (self.mousePos[0] - GRID_POSITION[0])//CELL_SIZE
+
+    def draw_grid(self, window):
         # draw border
-        pygame.draw.rect(window, BLACK, (GRID_POSITION[0], GRID_POSITION[1], GRID_WIDTH, GRID_HEIGHT), 2)
+        pygame.draw.rect(window, BLACK, (GRID_POSITION[0], GRID_POSITION[1], GRID_WIDTH, GRID_HEIGHT), 4)
         # draw lines
-        for x  in range(9):
-            pygame.draw.line(window, BLACK, (GRID_POSITION[0], (x+1)*CELL_SIZE+GRID_POSITION[1]), (GRID_POSITION[0]+GRID_WIDTH, (x+1)*CELL_SIZE+GRID_POSITION[1]), 2)
-            pygame.draw.line(window, BLACK, ((x+1)*CELL_SIZE+GRID_POSITION[0], GRID_POSITION[1]),((x+1)*CELL_SIZE+GRID_POSITION[0], GRID_POSITION[1]+GRID_HEIGHT), 2)
+        for x in range(1, 10):
+            if x % 3:
+                line_weight = 1
+            else:
+                line_weight = 4
+            pygame.draw.line(window, BLACK, (x * CELL_SIZE + GRID_POSITION[0], GRID_POSITION[1]),
+                             (x * CELL_SIZE + GRID_POSITION[0], GRID_POSITION[1] + GRID_HEIGHT), line_weight)
 
-
+            pygame.draw.line(window, BLACK, (GRID_POSITION[0], x * CELL_SIZE + GRID_POSITION[1]),
+                             (GRID_POSITION[0] + GRID_WIDTH, x * CELL_SIZE + GRID_POSITION[1]), line_weight)
 
     def update(self):
-        pass
+        self.mousePos = pygame.mouse.get_pos()
+
+    def draw_selected_cell(self, window, selected_pos):
+        pygame.draw.rect(window, GREEN, (selected_pos[1]*CELL_SIZE+GRID_POSITION[0], selected_pos[0]*CELL_SIZE+GRID_POSITION[1], CELL_SIZE, CELL_SIZE))
 
     def draw(self):
         self.window.fill(WHITE)
-        self.drawGrid(self.window)
+        if self.selected:
+            self.draw_selected_cell(self.window, self.selected)
+        self.draw_grid(self.window)
         pygame.display.update()
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ HEIGHT = 800
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
+GREEN = (0, 150, 136)
 
 BOARD = [[0 for x in range(9)] for x in range(9)]
 GRID_POSITION = (170, 230)
